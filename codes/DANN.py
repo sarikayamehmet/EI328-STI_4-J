e71a12a2d7d78c084146e76bd00ee53779b3df30
cnlnpjhsy <61415268+cnlnpjhsy@users.noreply.github.com>
2022-05-22 13:31:12 +0800

    check

--- codes/DANN.py
+++ codes/DANN.py
@@ -17,8 +17,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     p = parser.add_argument_group("Model")
-    p.add_argument("--load_path", type=str, default='./saved_models/LSTMbaseline')
-    p.add_argument("--save_path", type=str, default='./saved_models/LSTMbaseline')
+    p.add_argument("--load_path", type=str, default='./saved_models/DANN')
+    p.add_argument("--save_path", type=str, default='./saved_models/DANN')
 
     p = parser.add_argument_group("Train")
     p.add_argument("--batch_size", type=int, default=32)
@@ -52,10 +52,11 @@ if __name__ == '__main__':
     train_cnt = AverageMeter()
 
     eeg_data = _EEGDataset()
-    models = [DANN().to(device) for i in range(15)]
+    models = [DANN() for i in range(15)]
     loss_func = nn.CrossEntropyLoss()
     total_y_true, total_y_pred = np.empty(0, dtype=int), np.empty(0, dtype=int)
     for idx, model in enumerate(models):
+        model.to(device)
         x_train, y_train, x_test, y_test, d_source, d_target = eeg_data.leave_one_dataset(idx)
         train_data = DomainDataGenerator(x_train, y_train, d_source, x_test, d_target, seq_len=8)
         test_data = DomainDataGenerator(x_test, y_test, d_target, seq_len=8)
@@ -70,7 +71,6 @@ if __name__ == '__main__':
             for ep in range(1, args.epoch + 1):
                 train_cnt.reset()
 
-                hx, cx = torch.zeros(args.batch_size, 256), torch.zeros(args.batch_size, 256)
                 for i, batch in enumerate(train_loader):
                     train_cnt.update(0, 1)
                     p = float(train_cnt.count + ep * len(train_loader)) / (args.epoch * len(train_loader))
@@ -117,17 +117,17 @@ if __name__ == '__main__':
             y_pred = np.append(y_pred, pred)
 
         acc = accuracy_score(y_true, y_pred)
-        print('[LSTM baseline][Leave %d] Test done! On dataset #%d: Acc %.4f' % (idx, idx, acc))
+        print('[DANN][Leave %d] Test done! On dataset #%d: Acc %.4f' % (idx, idx, acc))
         total_y_true = np.append(total_y_true, y_true)
         total_y_pred = np.append(total_y_pred, y_pred)
 
     acc = accuracy_score(total_y_true, total_y_pred)
-    print('[LSTM baseline] All tests done! Total acc: %.4f' % acc)
+    print('[DANN] All tests done! Total acc: %.4f' % acc)
     disp = ConfusionMatrixDisplay.from_predictions(
         total_y_true,
         total_y_pred,
         normalize='all',
         values_format='.3f'
     )
-    plt.savefig('figures/LSTMbaseline.png')
+    plt.savefig('figures/DANN.png')
     plt.show()
